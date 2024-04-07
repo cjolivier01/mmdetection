6423ba98619085b2d51588f8a0967f77b020db70
Christopher Olivier <cjolivier01@gmail.com>
2024-04-07 08:38:47 -0700

    Add timing to detection

--- demo/video_demo.py
+++ demo/video_demo.py
@@ -4,11 +4,58 @@ import argparse
 import cv2
 import torch
 import mmcv
+import time
 from mmcv.transforms import Compose
 from mmengine.utils import track_iter_progress
 
 from mmdet.apis import inference_detector, init_detector
 from mmdet.registry import VISUALIZERS
+
+class TimerData:
+    def __init__(self, name: str = "Timer"):
+        self.name = name
+        self.reset()
+    
+    def reset(self):
+        self.counter = 0
+        self.accumulated_time = 0
+
+
+
+class Timer:
+    def __init__(self, timer_data: TimerData, batch_size: int = 1, print_interval: int = 25, start_with_carriage_return: bool = True):
+        self.print_interval = print_interval
+        self.batch_size = batch_size
+        self.start_with_carriage_return = start_with_carriage_return
+        self.timer_data = timer_data
+    
+    def reset(self):
+        self.timer_data.reset()
+    
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self, maybe_print: bool = True):
+        end_time = time.time()
+        self.timer_data.counter += 1
+        self.timer_data.accumulated_time += (end_time - self.start_time)
+        if maybe_print:
+            self.maybe_print_time()
+
+    def maybe_print_time(self, reset: bool = True):
+        if self.timer_data.counter % self.print_interval == 0:
+            frame_count = self.timer_data.counter * self.batch_size
+            fps = frame_count / self.timer_data.accumulated_time
+            cr = "\n"
+            print(f"{cr if self.start_with_carriage_return else ''}Processed {self.timer_data.counter} frames at {fps:.2f} fps")
+            if reset:
+                self.reset()
+
+    def __enter__(self):
+        self.start()
+
+    def __exit__(self, *args, **kwargs):
+        self.stop()
 
 
 def parse_args():
@@ -33,9 +80,9 @@ def parse_args():
 
 def main():
     args = parse_args()
-    assert args.out or args.show, \
-        ('Please specify at least one operation (save/show the '
-         'video) with the argument "--out" or "--show"')
+    # assert args.out or args.show, \
+    #     ('Please specify at least one operation (save/show the '
+    #      'video) with the argument "--out" or "--show"')
 
     # build the model from a config file and a checkpoint file
     model = init_detector(args.config, args.checkpoint, device=args.device)
@@ -59,24 +106,30 @@ def main():
             args.out, fourcc, video_reader.fps,
             (video_reader.width, video_reader.height))
 
+    timer_data = TimerData()
+
     for frame in track_iter_progress((video_reader, len(video_reader))):
-        result = inference_detector(model, frame, test_pipeline=test_pipeline)
+        with Timer(timer_data=timer_data, batch_size=1):
+            result = inference_detector(model, frame, test_pipeline=test_pipeline)
+            labels = result.pred_instances.labels.cpu()
+            bboxes = result.pred_instances.bboxes.cpu()
         if torch.is_floating_point(result.pred_instances.labels):
             result.pred_instances.labels = result.pred_instances.labels.to(torch.int64)
-        visualizer.add_datasample(
-            name='video',
-            image=frame,
-            data_sample=result,
-            draw_gt=False,
-            show=False,
-            pred_score_thr=args.score_thr)
-        frame = visualizer.get_image()
+        if args.out or args.show:
+            visualizer.add_datasample(
+                name='video',
+                image=frame,
+                data_sample=result,
+                draw_gt=False,
+                show=False,
+                pred_score_thr=args.score_thr)
+            frame = visualizer.get_image()
 
-        if args.show:
-            cv2.namedWindow('video', 0)
-            mmcv.imshow(frame, 'video', args.wait_time)
-        if args.out:
-            video_writer.write(frame)
+            if args.show:
+                cv2.namedWindow('video', 0)
+                mmcv.imshow(frame, 'video', args.wait_time)
+            if args.out:
+                video_writer.write(frame)
 
     if video_writer:
         video_writer.release()
